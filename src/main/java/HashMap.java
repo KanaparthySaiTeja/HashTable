59a80ae2c538ce7fb8f7878db11e21da27b70bfe
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-19 07:37:22 +0530

    count the frequency

---
+++ src/main/java/HashMap.java
@@ -0,0 +1,28 @@
+public class HashMap<K,V> {
+
+    LinkedList<K,V> list;
+
+    public HashMap(){
+        list=new LinkedList();
+    }
+
+    public V get(K key) {
+        NodeMap<K,V> mapNode = (NodeMap<K, V>) list.search(key);
+        return (mapNode == null) ? null : mapNode.getValue();
+    }
+
+    public void add(K key, V value) {
+        NodeMap<K,V> mapNode = (NodeMap<K, V>) list.search(key);
+        if(mapNode==null){
+            mapNode = new NodeMap<>(key,value);
+            list.append(mapNode);
+        }
+        else
+            mapNode.setValue(value);
+    }
+
+    @Override
+    public String toString() {
+        return "HashNodes {"+ list + "}";
+    }
+}
